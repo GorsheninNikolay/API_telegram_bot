8fa0cc206b5d966e6e3dfcded05b1fd5298457d9
Fr13ndyy <kolya.gorshenin.2002@gmail.com>
2021-06-02 09:01:08 +0300

    My Bot

--- homework.py
+++ homework.py
@@ -14,6 +14,7 @@ class WrongResponse(Exception):
 
 
 PRAKTIKUM_TOKEN = os.getenv("PRAKTIKUM_TOKEN")
+LEN_PRAKTIKUM_TOKEN = len(PRAKTIKUM_TOKEN)
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 BOT = telegram.Bot(token=TELEGRAM_TOKEN)
@@ -26,9 +27,7 @@ statuses = {
 }
 
 URL = {
-    "url": "https://praktikum.yandex.ru/api/user_api/homework_statuses/",
-    "headers": {"Authorization": f"OAuth {PRAKTIKUM_TOKEN}"},
-    "params": {"from_date": 0},
+    "url": "https://praktikum.yandex.ru/api/user_api/homework_statuses/"
 }
 
 logging.basicConfig(
@@ -43,41 +42,27 @@ def parse_homework_status(homework: dict) -> str:
     homework_name = homework.get("homework_name")
     status = homework.get("status")
     verdict = statuses.get(status)
-    if homework_name or status or verdict is None:
+    if (homework_name or status or verdict) is None:
         logging.error(f"Wrong server response.\nWith: {URL}")
-        # Pytest ругается из-за raise =(
-        # До этого еще пытался вернуть строкой: return "Неверный ответ сервера"
-        # Но Pytest все равно не пропускает.
-        # Также любое изменение Русских строк,
-        # Например, на английский язык, не принимает.
-        # Я так понимаю, что в тестах parse_homework_status
-        # Значение homework_name = None, поэтому функция, в моем случае,
-        # Возвращает исключение, Pytest сравнивает все assert-ом
-        # И вместо f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
-        # У меня выходит свое исключение.
-        """Вот ошибка с попыткой return "Неверный ответ сервера":
- AssertionError:
-Проверьте, что возвращаете название домашней
-                        работы в возврате функции parse_homework_status()
-E assert False
-E + where False = <built-in method startswith of str object at 0x04512E90>
-                            ('У вас проверили работу "FqGyDvIpxOcreLy"')
-E + where <built-in method startswith of str object at 0x04512E90>
-= 'Неверный ответ сервера'.startswith """
-        # raise WrongResponse(f"Wrong server response.\nWith: {URL}")
-        # return "Неверный ответ сервера"
+        raise WrongResponse(f"Wrong server response.\nWith: {URL}")
     return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
 def get_homework_statuses(current_timestamp: int) -> dict:
     try:
-        URL["params"]["from_date"] = current_timestamp
         homework_statuses = requests.get(
-            URL["url"],
-            headers=URL["headers"],
-            params=URL["params"])
+            URL.get("url"),
+            headers={"Authorization": f"OAuth {PRAKTIKUM_TOKEN}"},
+            params={"from_date": current_timestamp})
     except Exception as error:
-        logging.error(f"Request Error: {error}.\nWith: {URL}")
+        # Чтобы в лог не передавать весь токен,
+        # передаю в лог только 7 последних символов
+        logging.error(
+            f"Request Error: {error}."
+            "\nHeaders: Authorization: "
+            f"OAuth {'*'*(LEN_PRAKTIKUM_TOKEN - 7)}"
+            f"{PRAKTIKUM_TOKEN[LEN_PRAKTIKUM_TOKEN - 7: LEN_PRAKTIKUM_TOKEN]}"
+            f"\nParams: from_date: {current_timestamp}")
         return {}
     return homework_statuses.json()
 
